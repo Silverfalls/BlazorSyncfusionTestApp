d2b0dfa51efe7cc89c60abbd47bb76cbb0bb3a6a
Alexander Schröders <alexander.schroeders@googlemail.com>
2020-05-22 12:51:25 +0200

    Projektdateien hinzufügen.

---
+++ BlazorSyncfusionTestApp/Startup.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using BlazorSyncfusionTestApp.Data;
+using Microsoft.AspNetCore.Localization;
+using Syncfusion.Blazor;
+
+namespace BlazorSyncfusionTestApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddSyncfusionBlazor();
+            services.AddSingleton(typeof(ISyncfusionStringLocalizer), typeof(SyncfusionLocalizer));
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                // define the list of cultures your app will support
+                var supportedCultures = new List<CultureInfo>()
+                {
+                    new CultureInfo("de")
+                };
+                // set the default culture
+                options.DefaultRequestCulture = new RequestCulture("de");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.RequestCultureProviders = new List<IRequestCultureProvider>() {
+                    new QueryStringRequestCultureProvider() // Here, You can also use other localization provider
+                };
+            });
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRequestLocalization();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
---
+++ BlazorSyncfusionTestApp/SyncfusionLocalizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Syncfusion.Blazor;
+
+namespace BlazorSyncfusionTestApp
+{
+    public class SyncfusionLocalizer : ISyncfusionStringLocalizer
+    {
+        public string Get(string key)
+        {
+            return this.Manager.GetString(key);
+        }
+
+        public System.Resources.ResourceManager Manager
+        {
+            get
+            {
+                return BlazorSyncfusionTestApp.Resources.SfResources.ResourceManager;
+            }
+        }
+    }
+}
